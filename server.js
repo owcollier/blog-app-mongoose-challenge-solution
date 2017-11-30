cec6769a41f03e0c498d03423d4cf9b476fecb4a
Owcollier <owcollier@gmail.com>
2017-11-30 16:20:21 -0500

    added local strategy for authentication and installed passport dependencies

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const morgan = require('morgan');
 const {DATABASE_URL, PORT} = require('./config');
 const {BlogPost, User } = require('./models');
 
+const passport = require('passport');
+const { Strategy: LocalStrategy } = require('passport-local');
+
 const app = express();
 
 app.use(morgan('common'));
@@ -15,6 +18,46 @@ app.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
 
+// Local strategy for auth
+
+const localStrategy = new LocalStrategy((username, password, done) => {
+  let user;
+  User
+    .findOne({ username })
+    .then(results => {
+      user = results;    
+      
+      if (!user) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username',
+          location: 'username'
+        });
+      }    
+    
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect password',
+          location: 'password'
+        });
+      }
+      return done(null, user);    
+    })
+    .catch(err => {
+      if (err.reason === 'LoginError') {
+        return done(null, false);
+      }
+
+      return done(err);
+
+    });
+});
+
+passport.use(localStrategy);
 
 app.get('/posts', (req, res) => {
   BlogPost
